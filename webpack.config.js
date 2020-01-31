acccee93c07036ef527c4a75acb2584690e80770
Duong Nguyen <duongital@gmail.com>
2020-01-31 16:44:56 +0700

    init project

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin"); //installed via npm
+const webpack = require("webpack"); //to access built-in plugins
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: "./script.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: "./index.html" })]
+};
